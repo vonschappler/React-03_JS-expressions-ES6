18e5a40e3f3b0d656cc4d95c9d06980291a6ec03
von Schappler <vonschappler_dev@outlook.com>
2023-04-05 13:07:26 +0000

    Add second variable

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const name = 'Manoel'
+const number = 7
 
-ReactDOM.render(<h1>Hello {name}!</h1>, document.getElementById("root"));
+ReactDOM.render(
+  <div>
+    <h1>Hello {name}</h1>
+    <p>Your lucky number is {number}</p>
+  </div>,
+  document.getElementById("root")
+)
