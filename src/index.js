7503be60fc969e19eb4838b00d77385c6383cac7
von Schappler <vonschappler_dev@outlook.com>
2023-04-05 13:16:36 +0000

    Add more variables and expression

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const name = 'Manoel'
-const number = 7
+const fName = 'Manoel'
+const lName = 'Viturino'
+//const number = 7
 
 ReactDOM.render(
   <div>
-    <h1>Hello {name}</h1>
-    <p>Your lucky number is {number}</p>
+    <h1>Hello {`${fName} ${lName}`}</h1>
+    <p>Your lucky number is {Math.round(Math.random() * 10)}</p>
   </div>,
   document.getElementById("root")
 )
