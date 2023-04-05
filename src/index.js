7e0cc2155e09f632a0558789fbffc6e0dad51362
von Schappler <vonschappler_dev@outlook.com>
2023-04-05 13:04:50 +0000

    Update with simple variables

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const name = 'Manoel'
+
+ReactDOM.render(<h1>Hello {name}!</h1>, document.getElementById("root"));
